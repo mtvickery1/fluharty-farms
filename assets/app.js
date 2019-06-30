fbcf5732c3452d8f58939ab8f77a2a274a79fa4d
mv04 <mtvickery1@gmail.com>
2019-06-30 16:22:28 -0500

    switched slick with chocolate.js

--- assets/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $('.slick').slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
-    fade: true,
-    arrows: false
-  });
-});
---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+$('body').chocolate({
+  images: ['assets/images/drone/DJI_0181.JPG', 'assets/images/drone/DJI_0180.JPG'],
+  interval: 3000,
+  speed: 1000
+});
+
