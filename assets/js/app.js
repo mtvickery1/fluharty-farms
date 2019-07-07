ca749646e79afa7c5976d26ea95dac01732258e5
mv04 <mtvickery1@gmail.com>
2019-07-06 22:11:28 -0500

    homepage: navbar, description, basic styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-$('body').chocolate({
+$('#main-background').chocolate({
   images: ['assets/images/drone/DJI_0181.JPG', 'assets/images/drone/DJI_0180.JPG'],
-  interval: 3000,
+  interval: 4000,
   speed: 1000
 });
 
